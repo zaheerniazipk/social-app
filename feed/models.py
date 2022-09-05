0687d37f49ab1dcc9d0978d5ec13357357ec5c31
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-09-05 11:36:52 +0500

    Added DateTimeField in models

--- feed/models.py
+++ feed/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     text = models.CharField(max_length=240)
+    date = models.DateTimeField(auto_now=True)
 
     # return post text instead of post object
     def __str__(self):
