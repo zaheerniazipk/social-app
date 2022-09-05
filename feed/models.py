9586b0ee3b9a5d11da96bacc691c5874108913b7
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-09-05 11:32:47 +0500

    Registered model with Django admin

--- feed/models.py
+++ feed/models.py
@@ -4,3 +4,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     text = models.CharField(max_length=240)
+
+    # return post text instead of post object
+    def __str__(self):
+        return self.text[0:100]
