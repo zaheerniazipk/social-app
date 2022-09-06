b04ede594fbaa06c977aa0df7939702448213b2f
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-09-06 16:26:54 +0500

    Added author to Posts

--- feed/models.py
+++ feed/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User  # For creating User
 
 
 # Create your models here.
 class Post(models.Model):
     text = models.CharField(max_length=240)
     date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
 
     # return post text instead of post object
     def __str__(self):
