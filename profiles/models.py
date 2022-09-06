64640471a9ca927e6ac999343d322854d1c42b30
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-09-06 12:11:50 +0500

    Create user profiles

---
+++ profiles/models.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User  # For creating new user
+from django.db import models
+
+# Django signals
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='profile'
+    )
+
+    # render username instead of objects
+    def __str__(self):
+        return self.user.username
+
+
+# Django Signals
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """ Create a new Profile() object when a new Django user is created! """
+    if created:
+        Profile.objects.create(user=instance)
--- social_app/settings.py
+++ social_app/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
     # newly created apps
     'feed',
+    'profiles',
 ]
 
 MIDDLEWARE = [
