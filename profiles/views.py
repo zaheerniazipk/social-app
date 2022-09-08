48509d192dfebebee04b0acb09f29e97633b5932
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-09-08 18:17:45 +0500

    Get total posts by a user and display them

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
 
+from feed.models import Post
 from .models import Profile
 
 
@@ -13,3 +14,10 @@ class ProfileDetailView(DetailView):
     context_object_name = "user"
     slug_field = "username"
     slug_url_kwarg = "username"
+
+    # get total posts and followers
+    def get_context_data(self, **kwargs):
+        user = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context['total_posts'] = Post.objects.filter(author=user).count()
+        return context
