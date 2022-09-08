357de5f66eae62eade6884c2df60128099b6f5d7
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-09-08 18:04:56 +0500

    Added Profile Detail Template

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from . import views
 
-
-# namespace
-app_name = 'profiles'
-
+app_name = "profiles"
 
 urlpatterns = [
-    path('<str:username>/', views.ProfileDetailView.as_view(), name='detail'),
+    path("<str:username>/", views.ProfileDetailView.as_view(), name="detail"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -7,9 +7,9 @@ from .models import Profile
 
 # DetailView should be used when you want to present detail of a single model instance.
 class ProfileDetailView(DetailView):
-    http_method_names = ['get']
-    template_name = 'profiles/detail.html'
+    http_method_names = ["get"]
+    template_name = "profiles/detail.html"
     model = User
-    context_object_name = 'user'
-    slug_field = 'username'
-    slug_url_kwarg = 'username'
+    context_object_name = "user"
+    slug_field = "username"
+    slug_url_kwarg = "username"
