ec4acd033f30efa030fc7eb57dd3c8f5c82896c3
Zaheer Khan Niazi <zaheerniazipak@gmail.com>
2022-09-09 18:13:42 +0500

    Dynamic Homepage posts

--- feed/views.py
+++ feed/views.py
@@ -1,18 +1,48 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from feed.models import Post
+from followers.models import Follower
 
 
 # Create your views here.
 # Note: ListView should be used when to present a list of objects in a html page.
-class HomePage(ListView):
-    http_method_names = ['get']
-    template_name = 'index.html'
-    model = Post
-    context_object_name = 'posts'  # by default, it is objects
-    queryset = Post.objects.all().order_by('-id')[0:30]
+# class HomePage(ListView):
+#     http_method_names = ['get']
+#     template_name = 'index.html'
+#     model = Post
+#     context_object_name = 'posts'  # by default, it is objects
+#     queryset = Post.objects.all().order_by('-id')[0:30]
+
+
+# TemplateView should be used when you want to present some information in a html page
+# Show Posts by following
+class HomePage(TemplateView):
+    http_method_names = ["get"]
+    template_name = "index.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        if self.request.user.is_authenticated:
+            following = list(
+                Follower.objects.filter(followed_by=self.request.user).values_list(
+                    'following', flat=True)
+            )
+            if not following:
+                # Show the default 30
+                posts = Post.objects.all().order_by('-id')[0:30]
+            else:
+                posts = Post.objects.filter(
+                    author__in=following).order_by('-id')[0:60]
+        else:
+            posts = Post.objects.all().order_by('-id')[0:30]
+        context['posts'] = posts
+        return context
 
 
 # DetailView should be used when to present detail of a single model instance.
